d8c3a5cee7a196d0624db0c701b674f7105acb8e
redpeacock78 <scarletpeacock78@gmail.com>
2024-11-20 01:40:52 +0900

    fix: Fix secrets.ts

--- secrets.ts
+++ secrets.ts
@@ -1,18 +1,32 @@
+import { KEY_NAMES } from "./constants.ts";
 import UniEnv from "npm:@redpeacock78/unienv";
 
-const cryptoKey = UniEnv.get("CRYPTO_KEY");
-const digitKey = UniEnv.get("DIGIT_KEY");
-const discordToken = UniEnv.get("DISCORD_TOKEN");
-
-if (cryptoKey.isNg()) throw cryptoKey.error;
-if (digitKey.isNg()) throw digitKey.error;
-if (!cryptoKey.value) throw new Error("CRYPTO_KEY is empty");
-if (!digitKey.value) throw new Error("DIGIT_KEY is empty");
-if (discordToken.isNg()) throw discordToken.error;
-if (!discordToken.value) throw new Error("DISCORD_TOKEN is empty");
-
-export const Keys = {
-  crypto: cryptoKey.value,
-  digit: digitKey.value,
-  discord: discordToken.value,
+const handleEnvError = (missingEnvs: string[]): never => {
+  console.error(`Missing env variables: ${missingEnvs.join(", ")}`);
+  console.error(
+    `Please ensure these variables are set in your .env file or environment configuration.`
+  );
+  throw new Error(`Env check failed: ${missingEnvs.join(", ")}`);
 };
+
+const envs = (Object.keys(KEY_NAMES) as Array<keyof typeof KEY_NAMES>).map(
+  (key) => ({
+    name: KEY_NAMES[key],
+    alias: key.toLowerCase(),
+    result: UniEnv.get(KEY_NAMES[key]),
+  })
+);
+const hasEnvs: Record<string, string> = {};
+const notSetEnvsNames: string[] = envs.flatMap((env): string[] => {
+  if (env.result.isNg() || !env.result.value) return [env.name];
+  hasEnvs[env.alias] = env.result.value;
+  return [];
+});
+if (notSetEnvsNames.length > 0) handleEnvError(notSetEnvsNames);
+
+export const Keys = envs.reduce((acc, key): Record<string, string> => {
+  acc[key.alias] = hasEnvs[key.alias];
+  return acc;
+}, {} as Record<string, string>);
+
+console.log(Keys);
