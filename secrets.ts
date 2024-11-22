e9961305ccbf90c6cd337c1e8b0e9dcc75397620
redpeacock78 <scarletpeacock78@gmail.com>
2024-11-22 20:14:43 +0900

    fix: Fix secrets.ts

--- secrets.ts
+++ secrets.ts
@@ -1,37 +1,44 @@
 import { KEY_NAMES } from "./constants.ts";
-import UniEnv, { Result, Maybe, VersionError } from "npm:@redpeacock78/unienv";
+import UniEnv, { Result, Maybe, SomeError } from "npm:@redpeacock78/unienv";
 
 type KeyObj = typeof KEY_NAMES;
 type KeyType = keyof KeyObj;
-type GetResult = Result<Maybe<string>, VersionError | Error>;
-type EnvsType = {
+type EnvsType = Readonly<{
   name: KeyObj[KeyType];
   alias: KeyType;
-  result: GetResult;
-};
+  result: Result<Maybe<string>, SomeError>;
+}>;
+type EnvsNameType = EnvsType["name"];
+type KeysObj = Readonly<{
+  [key in KeyType]: string;
+}>;
 
-const handleEnvError = (missingEnvs: string[]): never => {
-  console.error(`Missing env variables: ${missingEnvs.join(", ")}`);
+const handleEnvError = (missingEnvsNames: readonly string[]): never => {
+  console.error(`Missing env variables: ${missingEnvsNames.join(", ")}`);
   console.error(
     `Please ensure these variables are set in your .env file or environment configuration.`
   );
-  throw new Error(`Env check failed: ${missingEnvs.join(", ")}`);
+  throw new Error(`Env check failed: ${missingEnvsNames.join(", ")}`);
 };
 
 const envs = (Object.keys(KEY_NAMES) as Array<KeyType>).map(
-  (key: KeyType): EnvsType => ({
-    name: KEY_NAMES[key],
-    alias: key,
-    result: UniEnv.get(KEY_NAMES[key]),
-  })
+  (key: KeyType) =>
+    ({
+      name: KEY_NAMES[key],
+      alias: key,
+      result: UniEnv.get(KEY_NAMES[key]),
+    } as const satisfies EnvsType)
 );
-const hasEnvs = {} as const as Record<KeyType, string>;
-const notSetEnvsNames: string[] = envs.flatMap((env): string[] => {
-  if (env.result.isNg() || !env.result.value) return [env.name];
-  hasEnvs[env.alias] = env.result.value;
-  return [];
-});
+const hasEnvs = {} as Record<KeyType, string>;
+const missingEnvsNames = envs.flatMap(
+  (env: EnvsType): readonly EnvsNameType[] | [] => {
+    if (env.result.isNg() || !env.result.value)
+      return [env.name] as const satisfies readonly EnvsNameType[];
+    hasEnvs[env.alias] = env.result.value;
+    return [];
+  }
+) as readonly EnvsNameType[];
 
-if (notSetEnvsNames.length > 0) handleEnvError(notSetEnvsNames);
+if (missingEnvsNames.length > 0) handleEnvError(missingEnvsNames);
 
-export const Keys = Object.freeze(hasEnvs);
+export const Keys = Object.freeze(hasEnvs) as KeysObj;
