e1279040a946e7fa4c99cd3af6581ac464606d12
redpeacock78 <scarletpeacock78@gmail.com>
2024-12-26 16:52:40 +0900

    fix: Fix libs.ts

--- libs.ts
+++ libs.ts
@@ -3,16 +3,38 @@ import crypto from "npm:crypto-js";
 import fastJson from "npm:fast-json-stringify";
 
 export const Crypto = {
-  encrypt: (data: string, key: string) => {
-    return crypto.AES.encrypt(data, key).toString();
-  },
-  decrypt: (data: string, key: string) => {
-    return crypto.AES.decrypt(data, key).toString(crypto.enc.Utf8);
-  },
-  genDigit: (data: string, key: string) =>
+  /**
+   * Encrypts a string using AES algorithm.
+   * @param {string} data The string to be encrypted.
+   * @param {string} key The encryption key.
+   * @returns {string} The encrypted string.
+   */
+  encrypt: (data: string, key: string): string =>
+    crypto.AES.encrypt(data, key).toString(),
+  /**
+   * Decrypts an AES-encrypted string.
+   * @param {string} data The encrypted string to be decrypted.
+   * @param {string} key The decryption key.
+   * @returns {string} The decrypted string in UTF-8 encoding.
+   */
+  decrypt: (data: string, key: string): string =>
+    crypto.AES.decrypt(data, key).toString(crypto.enc.Utf8),
+  /**
+   * Generates a digital signature for the given data using HMAC SHA-256 algorithm.
+   * @param {string} data The data to be signed.
+   * @param {string} key The signature key.
+   * @returns {string} The digital signature.
+   */
+  genDigit: (data: string, key: string): string =>
     crypto.HmacSHA256(data, key).toString(),
 };
 export const fJSON = {
+  /**
+   * Stringifies an object to a JSON string according to the given schema.
+   * @param {fastJson.AnySchema} schema The JSON schema to be used for stringification.
+   * @param {TDoc} doc The object to be stringified.
+   * @returns {string} The JSON string representation of the object.
+   */
   stringify: <TDoc extends object = object>(
     schema: fastJson.AnySchema,
     doc: TDoc
