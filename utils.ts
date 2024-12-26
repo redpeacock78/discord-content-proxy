a0f8a1653e242cef4b9c9601f5938602b998ca76
redpeacock78 <scarletpeacock78@gmail.com>
2024-12-26 16:52:55 +0900

    fix: Fix utils.ts

--- utils.ts
+++ utils.ts
@@ -1,9 +1,19 @@
 export const Base64Url = {
+  /**
+   * Encodes a string using Base64url.
+   * @param {string} str The string to be encoded.
+   * @returns {string} The Base64url encoded string.
+   */
   encode: (str: string): string =>
     btoa(String.fromCharCode(...Array.from(new TextEncoder().encode(str))))
       .replace(/\+/g, "-")
       .replace(/\//g, "_")
       .replace(/=/g, ""),
+  /**
+   * Decodes a Base64url encoded string.
+   * @param {string} str The Base64url encoded string to be decoded.
+   * @returns {string} The decoded string.
+   */
   decode: (str: string): string =>
     new TextDecoder().decode(
       Uint8Array.from(
