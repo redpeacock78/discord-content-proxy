4377ffc34722bba80a47858a37abb44f4d040538
redpeacock78 <scarletpeacock78@gmail.com>
2024-12-27 16:35:38 +0900

    fix: Fix constants.ts

--- constants.ts
+++ constants.ts
@@ -5,27 +5,6 @@ export const API_URL = new URL(
   "https://discord.com/api/v9/attachments/refresh-urls"
 );
 export const KEY_NAMES = ["DIGIT_KEY", "CRYPTO_KEY", "DISCORD_TOKEN"] as const;
-export const JSON_SCHEMA_OBJ = {
-  title: "JSON Schema",
-  type: "object",
-  properties: {
-    channelId: {
-      type: "string",
-    },
-    messageId: {
-      type: "string",
-    },
-    contentName: {
-      type: "string",
-    },
-    originalFileName: {
-      type: "string",
-    },
-    expiredAt: {
-      type: "string",
-    },
-  },
-} as const;
 export const JSON_SCHEMA = z.object({
   channelId: z.string(),
   messageId: z.string(),
