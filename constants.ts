73d3acd48a8dc1a808ca3369980f33556f3e2ace
redpeacock78 <scarletpeacock78@gmail.com>
2025-01-04 01:45:09 +0900

    fix: Fix constants.ts

--- constants.ts
+++ constants.ts
@@ -1,4 +1,5 @@
 import { z } from "npm:zod";
+import StatusCode from "npm:hono/utils";
 
 export const BASE_URL = new URL("https://cdn.discordapp.com");
 export const API_URL = new URL(
@@ -12,3 +13,13 @@ export const JSON_SCHEMA = z.object({
   originalFileName: z.string().optional(),
   expiredAt: z.string().optional(),
 });
+export const HTTP_STATUS = {
+  OK: 200,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+} as const satisfies {
+  [key: string]: StatusCode;
+};
