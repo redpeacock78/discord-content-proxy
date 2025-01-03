974255a5ede8396dabb1353f09f1c7a3c73d74d3
redpeacock78 <scarletpeacock78@gmail.com>
2025-01-04 02:24:12 +0900

    fix: Fix utils.ts

--- utils.ts
+++ utils.ts
@@ -1,3 +1,5 @@
+import { KyError, ErrorType } from "./types.ts";
+
 export const Base64Url = {
   /**
    * Encodes a string using Base64url.
@@ -21,4 +23,28 @@ export const Base64Url = {
         (c: string): number => c.charCodeAt(0)
       )
     ),
+};
+
+export const Guards = {
+  /**
+   * Checks if the given error is an instance of KyError.
+   * @param e The error to be checked.
+   * @returns True if the error is an instance of KyError, false otherwise.
+   * @example
+   * const error = new Error("Test");
+   * isKyError(error); // => false
+   * const kyError = ky.createError(error);
+   * isKyError(kyError); // => true
+   */
+  isKyError: (e: ErrorType): e is KyError =>
+    typeof e === "object" &&
+    e !== null &&
+    typeof (e as KyError).name === "string" &&
+    typeof (e as KyError).response === "object" &&
+    (e as KyError).response !== null &&
+    (typeof (e as KyError).response.code === "number" ||
+      typeof (e as KyError).response.code === "string") &&
+    typeof (e as KyError).response.title === "string" &&
+    typeof (e as KyError).response.status === "number" &&
+    typeof (e as KyError).response.reason === "string",
 };
