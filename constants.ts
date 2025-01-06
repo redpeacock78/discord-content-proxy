1a49ecb6e4a9337bbd5f741e48ea08f71131cb9f
redpeacock78 <scarletpeacock78@gmail.com>
2025-01-06 22:36:16 +0900

    fix: Fix constants.ts

--- constants.ts
+++ constants.ts
@@ -5,6 +5,7 @@ export const BASE_URL = new URL("https://cdn.discordapp.com");
 export const API_URL = new URL(
   "https://discord.com/api/v9/attachments/refresh-urls"
 );
+export const DIVISOR_TARGET = 50;
 export const KEY_NAMES = [
   "DIGIT_KEY",
   "CRYPTO_KEY",
