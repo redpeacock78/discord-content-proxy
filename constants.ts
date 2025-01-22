cfc67b8bf2be3ee742fc2334b1c4ebf33dec3fa3
redpeacock78 <scarletpeacock78@gmail.com>
2025-01-22 17:40:36 +0900

    feat: Update constants.ts

--- constants.ts
+++ constants.ts
@@ -6,11 +6,15 @@ export const API_URL = new URL(
   "https://discord.com/api/v9/attachments/refresh-urls"
 );
 export const DIVISOR_TARGET = 50;
+export const MAX_SEGMENT_SIZE = 9437184; // 9MB
 export const KEY_NAMES = [
   "DIGIT_KEY",
   "CRYPTO_KEY",
   "DISCORD_TOKEN",
   "IMG_SECRET",
+  "DISCORD_WEBHOOK_URL_1",
+  "DISCORD_WEBHOOK_URL_2",
+  "DISCORD_WEBHOOK_URL_3",
 ] as const;
 export const IMG_TYPES = {
   JPG: "image/jpeg",
@@ -18,11 +22,22 @@ export const IMG_TYPES = {
 } as const;
 export const VALID_IMG_TYPES = [IMG_TYPES.JPG, IMG_TYPES.PNG] as const;
 export const JSON_SCHEMA = z.object({
-  channelId: z.string(),
-  messageId: z.string(),
-  contentName: z.string(),
+  channelId: z.string().optional(),
+  messageId: z.string().optional(),
+  contentName: z.string().optional(),
+  contentType: z.string().optional(),
   originalFileName: z.string().optional(),
   expiredAt: z.string().optional(),
+  segments: z
+    .array(
+      z.object({
+        channelId: z.string(),
+        messageId: z.string(),
+        contentName: z.string(),
+        segmentIndex: z.number(),
+      })
+    )
+    .optional(),
 });
 export const HTTP_STATUS = {
   OK: 200,
